9de96763eeb957f193c65500ce33a6802aabbde9
ammar <ammarabdo441@gmail.com.com>
2023-01-25 22:06:46 +0300

    Implement Pagenation in Controller Countries v1

--- HotelListing.API/Repository/CountryRepository.cs
+++ HotelListing.API/Repository/CountryRepository.cs
@@ -1,22 +1,23 @@
-﻿using HotelListing.API.Data;
+﻿using AutoMapper;
+using HotelListing.API.Data;
 using HotelListing.API.IRepository;
 using HotelListing.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Repository
 {
-    public class CountryRepository : GenericRepository<Country>, ICountryRepository
-    {
-        private readonly HotelListingDbContext _context;
+	public class CountryRepository : GenericRepository<Country>, ICountryRepository
+	{
+		private readonly HotelListingDbContext _context;
 
-        public CountryRepository(HotelListingDbContext context) : base(context)
-        {
-            this._context = context;
-        }
+		public CountryRepository(HotelListingDbContext context, IMapper mapper) : base(context, mapper)
+		{
+			this._context = context;
+		}
 
-        public async Task<Country> GetDetailsAsync(int id)
-        {
-            return await _context.Countries.Include(m => m.Hotels).FirstOrDefaultAsync(m => m.Id == id);
-        }
-    }
+		public async Task<Country> GetDetailsAsync(int id)
+		{
+			return await _context.Countries.Include(m => m.Hotels).FirstOrDefaultAsync(m => m.Id == id);
+		}
+	}
 }
--- HotelListing.API/Repository/HotelRepository.cs
+++ HotelListing.API/Repository/HotelRepository.cs
@@ -1,16 +1,15 @@
-﻿using HotelListing.API.Data;
+﻿using AutoMapper;
+using HotelListing.API.Data;
 using HotelListing.API.IRepository;
 using HotelListing.API.Models;
 
 namespace HotelListing.API.Repository
 {
-    public class HotelRepository : GenericRepository<Hotel>, IHotelRepository
-    {
-        private readonly HotelListingDbContext _context;
+	public class HotelRepository : GenericRepository<Hotel>, IHotelRepository
+	{
 
-        public HotelRepository(HotelListingDbContext context) : base(context)
-        {
-            this._context = context;
-        }
-    }
+		public HotelRepository(HotelListingDbContext context, IMapper mapper) : base(context, mapper)
+		{
+		}
+	}
 }
